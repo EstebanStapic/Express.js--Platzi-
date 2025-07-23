c7e7a403fa3fe17c7985deaa8a5190a029b4f6d0
EstebanStapic <estebanstapic@gmail.com>
2025-07-23 11:07:54 -0300

    Uso de prisma y postgreSQL y middlewares en nodeJS

--- app.js
+++ app.js
@@ -1,19 +1,216 @@
-const dotenv = require('dotenv');
+require('dotenv').config();
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const loggerMiddleware = require('./middlewares/logger.js');
+const errorHandler = require('./middlewares/errorHandler.js');
+const { validateUser } = require('./utils/validation');
+const authenticateToken = require('./middlewares/auth.js');
+
+const bodyParser = require('body-parser');
+
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'users.json');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(loggerMiddleware);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
 
 app.get('/', (req, res) => {
     res.send(`
-        <h1>Curso Express.js V.3</h1>
-        <p>Esto es una aplicacion node.js con express.js</p>
+        <h1>Curso Express.js V3</h1>
+        <p>Esto es una aplicación node.js con express.js</p>
         <p>Corre en el puerto: ${PORT}</p>
-        `);
-})
-
-app.listen(PORT, () => {
-    console.log(`Servidor: http://localhost:${PORT}`);
+    `);
 });
 
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`Mostrar información del usuario con ID: ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+    const terms = req.query.termino || 'No especificado';
+    const category = req.query.categoria || 'Todas';
+
+    res.send(`
+        <h2>Resultados de Busqueda:</h2>
+        <p>Término: ${terms}</p>
+        <p>Categoría: ${category}</p>
+    `);
+});
+
+app.post('/form', (req, res) => {
+    const name = req.body.nombre || 'Anónimo';
+    const email = req.body.email || 'No proporcionado';
+    res.json({
+    message: 'Datos recibidos',
+        data: {
+        name,
+        email
+    }
+    });
+});
+
+app.post('/api/data', (req, res) => {
+    const data = req.body;
+
+    if (!data || Object.keys(data).length === 0) {
+    return res.status(400).json({ error: 'No se recibieron datos' });
+    }
+
+    res.status(201).json({
+    message: 'Datos JSON recibidos',
+    data
+    });
+});
+
+app.get('/users', (req, res) => {
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+    if (err) {
+        return res.status(500).json({ error: 'Error con conexión de datos.' });
+    }
+    const users = JSON.parse(data);
+    res.json(users);
+    });
+});
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+    if (err) {
+        return res.status(500).json({ error: 'Error con conexión de datos.' });
+    }
+    const users = JSON.parse(data);
+
+    const validation = validateUser(newUser, users);
+    if (!validation.isValid) {
+        return res.status(400).json({ error: validation.error });
+    }
+
+    users.push(newUser);
+    fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), err => {
+        if (err) {
+        return res.status(500).json({ error: 'Error al guardar el usuario.' });
+        }
+        res.status(201).json(newUser);
+    });
+    });
+});
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const updatedUser = req.body;
+
+    fs.readFile(usersFilePath, 'utf8', (err, data) => {
+    if (err) {
+        return res.status(500).json({ error: 'Error con conexión de datos.' });
+    }
+    let users = JSON.parse(data);
+
+    const validation = validateUser(updatedUser, users);
+    if (!validation.isValid) {
+        return res.status(400).json({ error: validation.error });
+    }
+
+    users = users.map(user =>
+        user.id === userId ? { ...user, ...updatedUser } : user
+    );
+    fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), err => {
+        if (err) {
+        return res
+        .status(500)
+        .json({ error: 'Error al actualizar el usuario' });
+    }
+        res.json(updatedUser);
+        });
+  });
+});
+
+app.delete('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json( { error: 'Error con conexión de datos.' });
+        }
+        let users = JSON.parse(data);
+        users = users.filter(user => user.id !== userId);
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error al eliminar el usuario.' });
+            }
+            res.status(204).send();
+        });
+    });
+});
+
+// [
+// Ruta de error para pruebas (http://localhost:3000/error)
+// app.get('/error', (req, res, next) =>{
+//     next(new Error('Este es un error de prueba'));
+// });
+//]
+
+app.get('/db-users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        res
+        .status(500)
+        .json({ error: 'Error al comunicarse con la base de datos.'});
+    }
+});
+
+app.get('/protected-route', authenticateToken, (req, res) => {
+    res.send('Esta es una ruta protegida');
+});
+
+app.post('/register', async (req, res) => {
+    const { email, password, name } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name,
+            role: 'USER'
+        }
+    });
+    res.status(201).json({ message: 'User Registered Successfully' });
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: { email }
+    });
+
+    if (!user) return res.status(400).json({error: 'Invalid email or password'});
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if(!validPassword) return res.status(400).json({error: 'Invalid email or password'});
+
+    const token = jwt.sign(
+        { id: user.id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+    
+    res.json({ token });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor Corriendo: http://localhost:${PORT}`);
+});
